b4c47b86a5978e68b5b48ce89fb41186ff937273
Juliet Ekuahorovwe <enoh@juliets-air.staff.staffs.ac.uk>
2025-12-23 15:09:52 +0000

    committed@1509 231225

--- app/dashboard/@Booking/page.jsx
+++ app/dashboard/@Booking/page.jsx
@@ -74,7 +74,7 @@ export default function page() {
   return (
     <>
     {
-        pathname === "/dashbroad" && booking ? (
+        booking && pathname === "/dashbroad" && booking ? (
         <div className='bookingPage'>
             <div className="bookingContainer">
                 <div className="bookingAccept" onClick={()=>{acceptBooking(booking)}}>
